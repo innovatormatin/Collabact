53355250e6bf06b5a1bd489a0686b29f33d92e9e
Manish <manojgeetparmar@gmail.com>
2022-09-25 21:11:55 +0530

    Section 1 with responsiveness complete

--- src/components/HomeComp/section1/Section1.js
+++ src/components/HomeComp/section1/Section1.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./section1.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import img1 from "../../../assets/img/banner/img1.png";
 import icon1 from "../../../assets/img/icons/img1.png";
 import icon2 from "../../../assets/img/icons/img2.png";
@@ -15,14 +17,74 @@ import {
   AiOutlineInstagram,
   AiFillYoutube,
 } from "react-icons/ai";
+import { IoIosArrowBack,
+  IoIosArrowForward } from "react-icons/io";
 import { FiTwitter } from "react-icons/fi";
 
 const Section1 = () => {
+  const responsive = {
+    tablet: {
+      breakpoint: { max: 960, min: 768 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 767, min: 0 },
+      items: 1,
+    },
+  };
+
+  const CustomButtonGroup = ({ next, previous, carouselState }) => {
+    const { totalItems, currentSlide } = carouselState;
+    return (
+      <div className="custom-button-group">
+        <button onClick={() => previous()}><IoIosArrowBack /></button>
+        <div>{currentSlide + 1} / {totalItems}</div>
+        <button onClick={() => next()}><IoIosArrowForward /></button>
+      </div>
+    );
+  };
   return (
     <div>
-      <section className="homeSection1">
+      <section className="homeSection1 container">
         <div className="homebanner">
           <img src={img1} alt="weConnect" />
+          <div className="advantage">
+            <h5>Advantages of associating with CollabAct</h5>
+            <div className="adcardContainer">
+              <div className="adcard">
+                <h6>Businesses</h6>
+                <i style={{ backgroundColor: "#0D72B8" }}>
+                  <img src={icon8} alt="img" />
+                  <AiFillYoutube />
+                </i>
+                <div>
+                  <strong>Global Sales</strong>
+                  <hr />
+                  <strong>Performance-based partners</strong>
+                  <hr />
+                  <strong>Low Expense, Increased Revenue</strong>
+                </div>
+                <button className="adb1">JOIN US</button>
+              </div>
+              <div className="adcard">
+                <h6>Sales Partners</h6>
+                <i style={{ backgroundColor: "#FFC114" }}>
+                  <img src={icon8} alt="img" />
+                  <AiFillYoutube />
+                </i>
+                <div>
+                  <strong>Flexible work Hours</strong>
+                  <hr />
+                  <strong>Commission-Based partnerships</strong>
+                  <hr />
+                  <strong>Zero investment & Unlimited income</strong>
+                </div>
+                <button className="adb2">
+                  <span>FREE</span> Registrations
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="bannerHeading">
             <h1>We Connect</h1>
             <h2>Commissioned Sales Partners</h2>
@@ -36,46 +98,9 @@ const Section1 = () => {
           <AiOutlineInstagram />
           <FiTwitter />
         </div>
-        <div className="advantage">
-          <h5>Advantages of associating with CollabAct</h5>
-          <div className="adcardContainer">
-            <div className="adcard">
-              <h6>Businesses</h6>
-              <i style={{ backgroundColor: "#0D72B8" }}>
-                <img src={icon8} alt="img" />
-                <AiFillYoutube />
-              </i>
-              <div>
-                <strong>Global Sales</strong>
-                <hr />
-                <strong>Performance-based partners</strong>
-                <hr />
-                <strong>Low Expense, Increased Revenue</strong>
-              </div>
-              <button className="adb1">JOIN US</button>
-            </div>
-            <div className="adcard">
-              <h6>Sales Partners</h6>
-              <i style={{ backgroundColor: "#FFC114" }}>
-                <img src={icon8} alt="img" />
-                <AiFillYoutube />
-              </i>
-              <div>
-                <strong>Flexible work Hours</strong>
-                <hr />
-                <strong>Commission-Based partnerships</strong>
-                <hr />
-                <strong>Zero investment & Unlimited income</strong>
-              </div>
-              <button className="adb2">
-                <span>FREE</span> Registrations
-              </button>
-            </div>
-          </div>
-        </div>
       </section>
       <section className="homeSection2">
-        <div>
+        <div className="container">
           <h3>Get started!</h3>
           <h2>
             With India's Fastest Growing <br /> Collaboration Platform
@@ -91,12 +116,12 @@ const Section1 = () => {
         </div>
       </section>
       <section className="homeSection3">
-        <div className="industrySegment">
+        <div className="industrySegment container">
           <span></span>
           <h2>
             Various <strong>Industry segments</strong>
           </h2>
-          <div className="industrySegmentGrid">
+          <div className="industrySegmentGrid ondeskSegment">
             <div className="segmentCard">
               <img src={icon7} alt="1" />
               <ul>
@@ -202,6 +227,120 @@ const Section1 = () => {
                 <li>Books & Toys</li>
               </ul>
             </div>
+          </div>
+          <div className="onMobSegment">
+            <Carousel
+              customButtonGroup={<CustomButtonGroup />}
+              renderButtonGroupOutside
+              arrows={false}
+              responsive={responsive}
+            >
+              <div className="segmentCard">
+                <img src={icon7} alt="1" />
+                <ul>
+                  <h5>Automobile and electric vehicle</h5>
+                  <li>Spare Parts & Equipments</li>
+                  <li>Accessories</li>
+                  <li>Electric Vehicles</li>
+                  <li>Fittings & Components</li>
+                  <li>Commercial Vehicles</li>
+                  <li>Battery</li>
+                </ul>
+              </div>
+              <div id="sepSegment" className="segmentCard">
+                <div className="b1">
+                  <img src={icon6} alt="2" />
+                  <ul>
+                    <h5>Industrial Products</h5>
+                    <li>Engineering Machines</li>
+                    <li>Tools & equipments</li>
+                    <li>Industrial Supplies</li>
+                  </ul>
+                </div>
+                <div className="b1">
+                  <img src={icon5} alt="2" />
+                  <ul>
+                    <h5>Fabric and Textiles</h5>
+                    <li>Apparel, Clothing & Garments</li>
+                    <li>Cotton, Wool Textiles & Fabrics</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="segmentCard">
+                <img src={icon7} alt="3" />
+                <ul>
+                  <h5>Business Services</h5>
+                  <li>Software, Technology, or IT</li>
+                  <li>Training, Coaching Consulting</li>
+                  <li>Human Resource & Legal</li>
+                  <li>Event Planning</li>
+                  <li>Insurance</li>
+                  <li>Advertisement & Media Support</li>
+                </ul>
+              </div>
+              <div className="segmentCard">
+                <img src={icon4} alt="4" />
+                <ul>
+                  <h5>Building construction and home products</h5>
+                  <li>Architectural & Civil Engineering</li>
+                  <li>Furniture & Supplies</li>
+                  <li>Ceramic & Flooring</li>
+                  <li>Home Decor</li>
+                  <li>Electronics & Electrical Goods</li>
+                </ul>
+              </div>
+              <div className="segmentCard">
+                <img src={icon3} alt="5" />
+                <ul>
+                  <h5>Energy and Enviornment</h5>
+                  <li>Paper & Paper products</li>
+                  <li>Solar Products</li>
+                  <li>Water Treatment</li>
+                  <li>Air Cleaning</li>
+                  <li>Waste Management</li>
+                </ul>
+              </div>
+              <div className="segmentCard">
+                <img src={icon5} alt="6" />
+                <ul>
+                  <h5>Hospital and medical equipments</h5>
+                  <li>Hospital Set-up Equipments</li>
+                  <li>Healthcare Products</li>
+                  <li>Food Supplements</li>
+                  <li>Medical Safety & protective Gears</li>
+                </ul>
+              </div>
+              <div className="segmentCard">
+                <img src={icon2} alt="7" />
+                <ul>
+                  <h5>Agriculture</h5>
+                  <li>Food & Beverages</li>
+                  <li>Agro Machinery & Equipments</li>
+                  <li>Seeds & Plantation Products</li>
+                  <li>Fertilisers & Soil Additives</li>
+                </ul>
+              </div>
+              <div className="segmentCard">
+                <img src={icon3} alt="8" />
+                <ul>
+                  <h5>Franchises</h5>
+                  <li>Sports & Fitness</li>
+                  <li>Laboratories</li>
+                  <li>Travel & Tourism</li>
+                  <li>Education</li>
+                </ul>
+              </div>
+              <div className="segmentCard">
+                <img src={icon1} alt="9" />
+                <ul>
+                  <h5>Fast Moving Consumer Goods (FMCG)</h5>
+                  <li>Food & Beverages</li>
+                  <li>Stationery</li>
+                  <li>Gifting</li>
+                  <li>Books & Toys</li>
+                </ul>
+              </div>
+            </Carousel>
           </div>
         </div>
       </section>
